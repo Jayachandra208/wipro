a8a8174969afc6fa9618b94ff663d44581bfc98d
KOPPALA  JAYACHANDRA REDDY <142087893+Jayachandra208@users.noreply.github.com>
2023-08-26 15:45:32 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,7 @@
+import java.io.*;
+class sample
+{
+public static void main(String arg[]){
+System.out.println("Hello\nAlexandra Abramov");
+}
+}
---
+++ sample2.java
@@ -0,0 +1,6 @@
+import java.io.*;
+ class sample2 {
+  public static void main(String[] args) {
+  System.out.println(74+36);
+ }
+ }
